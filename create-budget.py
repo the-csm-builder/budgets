e8af25f68a49187476ecb30096154ab0475875c3
John K <95291443+the-csm-builder@users.noreply.github.com>
2022-12-09 14:17:53 -0500

    Update create-budget.py

--- create-budget.py
+++ create-budget.py
@@ -1,72 +1,31 @@
 import boto3
-import json
-from datetime import datetime
 import csv
+from datetime import datetime
 
 client = boto3.client('budgets')
-
-# OPEN CSV LOCAL DRIVE
-
 
 def create_budget_from_csv():
     with open('Budget-Template.csv', 'r') as csvfile:
         datareader = csv.DictReader(csvfile)
         for row in datareader:
             response = client.create_budget(
-                AccountId='XXXX',  # Enter your Account ID
+                AccountId='XXXX',
                 Budget={
                     'BudgetName': row['BudgetName'],
                     'BudgetLimit': {
                         'Amount': row['BudgetAmount'],
                         'Unit': row['Currency']
                     },
-                    # 'PlannedBudgetLimits': {
-                    # 'string': {
-                    # 'Amount': 'string',
-                    # 'Unit': 'string'
-                    # }
-                    # },
                     'CostFilters': {
-                        'Region': [row['Filteredby']],
+                        'Service': [row['Filteredby']],
                     },
-                    # 'CostTypes': {
-                    #     'IncludeTax': True|False,
-                    #     'IncludeSubscription': True|False,
-                    #     'UseBlended': True|False,
-                    #     'IncludeRefund': True|False,
-                    #     'IncludeCredit': True|False,
-                    #     'IncludeUpfront': True|False,
-                    #     'IncludeRecurring': True|False,
-                    #     'IncludeOtherSubscription': True|False,
-                    #     'IncludeSupport': True|False,
-                    #     'IncludeDiscount': True|False,
-                    #     'UseAmortized': True|False
-                    # },
                     'TimeUnit': row['TimeUnit'],
                     'TimePeriod': {
                         'Start': row['BudgetStart'],
                         'End': row['BudgetEnd']
-                        # },
-                        # 'CalculatedSpend': {
-                        #     'ActualSpend': {
-                        #         'Amount': 'string',
-                        #         'Unit': 'string'
-                        #     },
-                        #     'ForecastedSpend': {
-                        #         'Amount': 'string',
-                        #         'Unit': 'string'
-                        #     }
                     },
                     'BudgetType': row['BudgetType'],
                     'LastUpdatedTime': row['CreatedDt'],
-                    # 'AutoAdjustData': {
-                    #     # 'AutoAdjustType': 'HISTORICAL'|'FORECAST',
-                    #     # 'HistoricalOptions': {
-                    #     # 'BudgetAdjustmentPeriod': 123,
-                    #     # 'LookBackAvailablePeriods': 123
-                    #     # },
-                    #     # 'LastAutoAdjustTime': datetime(2015, 1, 1)
-                    # }
                 },
                 NotificationsWithSubscribers=[
                     {
@@ -75,20 +34,16 @@ def create_budget_from_csv():
                             'ComparisonOperator': 'GREATER_THAN',
                             'Threshold': 80,
                             'ThresholdType': 'PERCENTAGE'
-                            # 'NotificationState': 'ALARM'
                         },
                         'Subscribers': [
                             {
-                                # Enter column name
                                 'SubscriptionType': row['NotificationType'],
-                                # enter column name
                                 'Address': row['NotificationEmail']
                             },
                         ]
                     },
                 ]
             )
-
 
 create_budget_from_csv()
 print('check your budget in console')
